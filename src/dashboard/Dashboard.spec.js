630d7f8e15fdb915cdeabe8ebbc754be8fbbf7f3
unknown <kyleteeter12@gmail.com>
2019-02-05 22:53:03 -0600

    Rendering dashboard check complete

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,13 @@
 // Test away
+import React from 'react';
+import {cleanup, render,} from "react-testing-library";
+import "jest-dom/extend-expect";
+import Dashboard from "./Dashboard";
+afterEach(cleanup);
+
+describe('The Display Component', () => {
+    it('renders display', () => {
+        render(<Dashboard />);
+    });
+
+});
